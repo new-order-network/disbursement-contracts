f7178d98516343ecb7e42ced1f16abe78dea499f
Arjan Villon <heaven.arjan@gmail.com>
2022-01-28 00:27:33 +0800

    Change the deploy-params and remove redundant disbursement test

--- deploy-params.js
+++ deploy-params.js
@@ -3,9 +3,8 @@ const endDate = new Date("March 01 2022 00:00:00 UTC");
 module.exports = {
   receiver: "0x3257Bde8CF067aE6f1DDc0E4b140fe02e3C5e44f", // The person/project receiving the grant
   wallet: "0x3257Bde8CF067aE6f1DDc0E4b140fe02e3C5e44f", // Wallet allowed to withdraw funds if criteria for the grant is not achieved
-  // disbursementPeriod: 3600*24*365, // Amount of time in seconds for the disbursement
   disbursementPeriod: Math.floor(endDate / 1000 - new Date() / 1000),
   startDate: 0, // Starting time for the distribution, (cliff)
-  cliffDate: 1000,
-  token: "0x6810e776880c02933d47db1b9fc05908e5386b96", // Token used for payout
+  cliffDate: 86400,
+  token: "0x6ADa033c7A28152BCD563d0B9a4B1d470fC8716e", // Token used for payout
 };
